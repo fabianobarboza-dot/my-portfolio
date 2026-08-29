4ced50dc07943da528a13bf1f2692db3aac41952
fabianobarboza-dot <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>
2026-08-29 00:12:47 -0300

    feat: configs of mui breakpoints

--- src/pages/Home/sections/Hero/hero.tsx
+++ src/pages/Home/sections/Hero/hero.tsx
@@ -1,31 +1,68 @@
-import { styled, Grid, Container, Typography } from "@mui/material";
+import {
+  styled,
+  Grid,
+  Typography,
+  Stack,
+  Button,
+  Container,
+} from "@mui/material";
 import avatar from "../../../../assets/images/avatar.jpeg";
+import DownloadIcon from "@mui/icons-material/Download";
+import EmailIcon from "@mui/icons-material/Email";
 
 const Hero = () => {
   const StyledHero = styled("div")(() => ({
     backgroundColor: "black",
-  }))
+    height: "100vh",
+  }));
 
   const StyledImg = styled("img")(() => ({
-        width: "30%",
-        borderRadius: "50%"
-  }))
+    width: "100%",
+    borderRadius: "50%",
+  }));
 
   return (
     <>
       <StyledHero>
-        <Container>
-            <Grid container spacing={2}>
-            <Grid item xs={4}>
-                <StyledImg src={avatar} />
+        <Container maxWidth="lg">
+          <Grid container spacing={2}>
+            <Grid size={{ xs: 12, md: 4 }}>
+              <StyledImg src={avatar} />
             </Grid>
-            <Grid item xs={8}>
-                <Typography color="primary">Fabiano Barboza</Typography>
-                <Typography color="primary">I'm a computer scientist</Typography>
+            <Grid size={{ xs: 12, md: 8 }}>
+              <Typography variant="h1" color="primary" textAlign="center">
+                Fabiano Barboza
+              </Typography>
+
+              <Typography variant="h2" color="primary" textAlign="center">
+                I'm a scientist computer
+              </Typography>
+              <Grid
+                container
+                sx={{ display: "flex", justifyContent: "center" }}
+              >
+                <Grid
+                  size={{ xs: 12, md: 4 }}
+                  sx={{ display: "flex", justifyContent: "center" }}
+                >
+                  <Button>
+                    <DownloadIcon />
+                    Download CV
+                  </Button>
+                </Grid>
+                <Grid
+                  size={{ xs: 12, md: 4 }}
+                  sx={{ display: "flex", justifyContent: "center" }}
+                >
+                  <Button>
+                    <EmailIcon />
+                    Contact ME
+                  </Button>
+                </Grid>
+              </Grid>
             </Grid>
-        </Grid>
+          </Grid>
         </Container>
-        
       </StyledHero>
     </>
   );
---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#232323',
+    },
+    secondary: {
+      main: '#4f8e3e',
+    },
+  },
+  typography: {
+
+}
+});
+
+export default theme;
